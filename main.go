96363646585cc706c63037cdd4c312b737858f05
Ivan Pedrazas <ipedrazas@gmail.com>
2016-08-12 01:28:18 +0200

    check if dir needs to be created

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -12,12 +13,28 @@ func check(e error) {
 	}
 }
 
+// exists returns whether the given file or directory exists or not
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
 func main() {
 	rand.Seed(time.Now().Unix())
 	numRes := flag.Int("n", 1, "Number of resources created")
 	baseTemplate := flag.String("t", "tmpl/dep.template", "Template used")
 	baseDir := flag.String("d", "runtest", "Target dir")
 	flag.Parse()
+
+	if _, err := os.Stat(*baseDir); os.IsNotExist(err) {
+		os.MkdirAll(*baseDir, 0755)
+	}
 
 	for i := 1; i < *numRes+1; i++ {
 		createService(i, *baseDir, *baseTemplate)
