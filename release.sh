7b1417ced3804b859b4c0e780c08042a50544402
Ivan Pedrazas <ipedrazas@gmail.com>
2016-09-12 16:52:22 +0100

    fix bug with release

--- release.sh
+++ release.sh
@@ -4,14 +4,14 @@ OSS=(darwin linux freebsd)
 ARCHS=(amd64 386)
 
 mkdir -p src
-mkdir -p bin
-rm -f bin/1024apis-*
+mkdir -p $WERCKER_OUTPUT_DIR/bin
+rm -f $WERCKER_OUTPUT_DIR/bin/1024apis-*
 
 for os in "${OSS[@]}"; do
     for arch in "${ARCHS[@]}"; do
     	echo "Building for $os($arch)"
         GOOS=$os GOARCH=$arch go build
-        mv 1024apis bin/1024apis-$os-$arch
+        mv 1024apis $WERCKER_OUTPUT_DIR/bin/1024apis-$os-$arch
     done
 done
 
